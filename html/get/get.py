3690f37545caf5db0cef6533ee2a489021099504
PurpleStripedUnicorn <dirck.code@gmail.com>
2019-12-12 15:18:26 +0100

    Added CPU usage measurement script

---
+++ html/get/get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import json
+import psutil
+
+# object that represents one measurement that has been done
+class Measure:
+
+    def __init__(self, title, value):
+        self.title = title
+        self.value = value
+
+# function that calculates the average of a list of numbers
+# input can be either 1 list object or multiple numbers
+def avg(*l):
+    if isinstance(l[0], list):
+        l = l[0]
+    return sum(l) / len(l)
+
+# returns the cpu usage times from last boot as a list of properties
+def measure_cpu():
+    dat = [entry.system + entry.user for entry in psutil.cpu_times(True)]
+    return [ Measure('cpu_times_total', avg(dat)),
+             Measure('cpu_times_cores', dat) ]
+
+dat = measure_cpu()
+print(json.dumps([entry.__dict__ for entry in dat]))
---
+++ html/get/index.php
@@ -0,0 +1,10 @@
+<?php
+
+# This file is only here to excute the python code in 'get.py'
+# This is aimed to be the only php code in the project, for ease of use this
+#   code is kept in as a bridge between python and the web server
+
+# prints the output of the 'get.py' script
+$output;
+exec('python3 get.py', $output);
+print($output);
